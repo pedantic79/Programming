1147f5868f047de7781a1d1702af049a5635e1b9
Dan Nguyen <pedantic@gmail.com>
2015-02-27 15:42:43 -0600

    Add comments

--- poss_avg/conf_avg.py
+++ poss_avg/conf_avg.py
@@ -5,9 +5,13 @@ import sys
 import re
 
 def avg(item):
+    """ Calculate the average"""
+
+    # item is (conf, (poss, games))
     return item[1][0]/item[1][1]
 
 def get_matches(filename):
+    """ Returns a list of tuples: (poss, team, conference, games) """
     f = open(filename, 'r')
     m = re.findall('<td.*?>(\d+\.\d+)</td>.*?' \
                    '<a href=.*?teams.*?>(.*?)</a>.*?' \
@@ -24,7 +28,7 @@ def read_stat(filename):
     for t in matches:
         name = t[2]
         team = t[1]
-        data = (int(float(t[0])), int(t[3]))
+        data = (int(float(t[0])), int(t[3])) # a tuple (poss, games)
 
         if name == 'Independents':
             name = 'I-' + team
@@ -32,8 +36,10 @@ def read_stat(filename):
         if not name in conf:
             conf[name] = data
         else:
+            # Add each individual tuple field for data to conf[name
             conf[name] = tuple([sum(x) for x in zip(conf[name], data)])
 
+    # sort based on the value returned by the average
     for i in sorted(conf.items(), key=avg, reverse=True):
         print('{0:25} {1:0.2f}'.format(i[0], avg(i)))
 
