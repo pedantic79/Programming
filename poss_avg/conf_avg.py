af7bba95e1af88b166bb855d1254f07daa2be873
Dan Nguyen <pedantic@gmail.com>
2015-02-27 16:51:03 -0600

    Figure out urlib and refactor

--- poss_avg/conf_avg.py
+++ poss_avg/conf_avg.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+# http://statsheet.com/mcb/teams/stats?season=2014-2015&conf=&games=&stat=possessions&stat_type=
 
 import sys
 import re
+import urllib.request
 
 def avg(item):
     return item[1][0]/item[1][1]
 
-def get_matches(filename):
-    f = open(filename, 'r')
+def get_matches(fd):
     m = re.findall('<td.*?>(\d+\.\d+)</td>.*?' \
                    '<a href=.*?teams.*?>(.*?)</a>.*?' \
                    '<a href=.*?conferences.*?>(.*?)</a>.*?' \
                    '<td>(\d+)</td>', 
-                   f.read(), re.MULTILINE | re.DOTALL)
-    f.close()
+                   fd.read(), re.MULTILINE | re.DOTALL)
     return m
 
 def read_stat(filename):
@@ -41,11 +41,17 @@ def read_stat(filename):
 def main():
     args = sys.argv[1:]
 
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36')]
+    opener.open('http://statsheet.com/mcb/teams/stats?season=2014-2015&conf=&games=&stat=possessions&stat_type=');
+
     if not args:
         print('usage: htmlfile')
         sys.exit(1)
 
-    read_stat(args[0])
+    f = open(args[0], 'r')
+    read_stat(f)
+    f.close()
 
 
 if __name__ == '__main__':
