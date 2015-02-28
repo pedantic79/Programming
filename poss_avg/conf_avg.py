c29c2b8fdf0179af14ec2882ffa981ebf3626b83
Dan Nguyen <pedantic@gmail.com>
2015-02-27 18:26:17 -0600

    Rework parsing function to work with both files and urls.

--- poss_avg/conf_avg.py
+++ poss_avg/conf_avg.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-# http://statsheet.com/mcb/teams/stats?season=2014-2015&conf=&games=&stat=possessions&stat_type=
 
 import sys
 import re
@@ -9,12 +8,12 @@ import urllib.request
 def avg(item):
     return item[1][0]/item[1][1]
 
-def get_matches(fd):
+def get_matches(str):
     m = re.findall('<td.*?>(\d+\.\d+)</td>.*?' \
                    '<a href=.*?teams.*?>(.*?)</a>.*?' \
                    '<a href=.*?conferences.*?>(.*?)</a>.*?' \
                    '<td>(\d+)</td>', 
-                   fd.read(), re.MULTILINE | re.DOTALL)
+                   str, re.MULTILINE | re.DOTALL)
     return m
 
 def read_stat(filename):
@@ -43,14 +42,15 @@ def main():
 
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36')]
-    opener.open('http://statsheet.com/mcb/teams/stats?season=2014-2015&conf=&games=&stat=possessions&stat_type=');
+    o=opener.open('http://statsheet.com/mcb/teams/stats?season=2014-2015&conf=&games=&stat=possessions&stat_type=');
+    read_stat(o.read().decode('utf-8'))
 
     if not args:
         print('usage: htmlfile')
         sys.exit(1)
 
     f = open(args[0], 'r')
-    read_stat(f)
+    read_stat(f.read())
     f.close()
 
 
