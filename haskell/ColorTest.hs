f74dfbcf31952593e41583242a3f061d9359920d
Dan Nguyen <pedantic@gmail.com>
2015-03-05 01:07:18 -0600

    Working version.

--- haskell/ColorTest.hs
+++ haskell/ColorTest.hs
@@ -1,28 +1,35 @@
-import Control.Monad
-import Text.Printf
-
-produceRGB c = [ (r,g,b) | b <- c, g <- c, r <- c ]
+module Main where
+import Control.Monad (when)
+import Text.Printf (printf)
 
 toHexColor :: (Integer, Integer, Integer) -> String
 toHexColor (r,g,b) = printf "%02x/%02x/%02x" r g b
 
 colors = produceRGB [0, 95, 135, 175, 215, 255]
+  where produceRGB l = [ (r,g,b) | r <- l, g <- l, b <- l ]
 
 simple = produceRGB [0, 191] ++ produceRGB [64, 255]
+  where produceRGB l = [ (r,g,b) | b <- l, g <- l, r <- l ]
 
--- putStr $ printf "\o33[38;5;%dHello\o33[0m" 10
--- mapM_ print $ map toHexColor simple
+grey = produceRGB [8, 18..240]
+  where produceRGB l = [ (c,c,c) | c <- l ]
 
-printColors :: [(Integer, Integer, Integer)] -> Integer -> IO()
-printColors colorList start = do
-  printColors' colorList 0
+printColors :: [(Integer, Integer, Integer)] -> Integer -> IO ()
+printColors colorList start = do printColors' colorList 0
   where
-    printColors' :: [(Integer, Integer, Integer)] -> Integer -> IO()
     printColors' (c:cs) i = do
-      putStr $ printf "\o33[48;5;%dm%s\o33[0m" (i + start) $ toHexColor c
-      when (mod i 5 == 4) $ putStrLn ""
+      printNum $ i + start
+      printVal (i + start) $ toHexColor c
+      when (mod i 6 == 5) $ putStrLn ""
       printColors' cs $ i + 1
-    printColors' _ _ = do
-      putStrLn ""
+    printColors' _ _ = do return ()
+    printNum num =
+      putStr $ printf "\o33[1;38;5;%dm%3s: \o33[0m" num $ show num
+    printVal color value =
+      putStr $ printf "\o33[38;5;%dm%s\o33[0m  " color value
 
-  
+main = do
+--  printColors simple 0
+--  putStrLn ""
+  printColors colors 16
+  printColors grey 232
