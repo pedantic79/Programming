9bd0ac06782ef2c1ce5694ff50500e0eeecd35c9
Dan Nguyen <pedantic@gmail.com>
2015-04-02 15:20:44 -0500

    Update benchmarks

--- haskell/ListPerf/Test.hs
+++ haskell/ListPerf/Test.hs
@@ -17,20 +17,20 @@ accApp m = accApp' 0 []
 simple m = [0..m]
 
 main = defaultMain
-       [ bgroup "accApp" [ bench "10k"  $ whnf accApp 10000
-                         , bench "50k"  $ whnf accApp 50000
-                         , bench "500k" $ whnf accApp 500000
-                         , bench "5m"   $ whnf accApp 5000000
+       [ bgroup "accApp" [ bench "1"   $ whnf accApp 1
+                         , bench "10"  $ whnf accApp 10
+                         , bench "50"  $ whnf accApp 50
+                         , bench "100" $ whnf accApp 100
                          ]
-       , bgroup "accPre" [ bench "10k"  $ whnf accPre 10000
-                         , bench "50k"  $ whnf accPre 50000
-                         , bench "500k" $ whnf accPre 500000
-                         , bench "5m"   $ whnf accPre 5000000
+       , bgroup "accPre" [ bench "1"   $ whnf accPre 1
+                         , bench "10"  $ whnf accPre 10
+                         , bench "50"  $ whnf accPre 50
+                         , bench "100" $ whnf accPre 100
                          ]
-       , bgroup "noAcc"  [ bench "10k"  $ whnf noAcc  10000
-                         , bench "50k"  $ whnf noAcc  50000
-                         , bench "500k" $ whnf noAcc  500000
-                         , bench "5m"   $ whnf noAcc  5000000
+       , bgroup "noAcc"  [ bench "1"   $ whnf noAcc  1
+                         , bench "10"  $ whnf noAcc  10
+                         , bench "50"  $ whnf noAcc  50
+                         , bench "100" $ whnf noAcc  100
                          ]
        ]
 
