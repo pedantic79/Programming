8ffe6f7ac08162feed80d47bcb0e1ba0e7a8d860
Dan Nguyen <pedantic@gmail.com>
2015-04-20 10:04:37 -0500

    Create datatype to hold our state

--- haskell/Jugglefest/Parse.hs
+++ haskell/Jugglefest/Parse.hs
@@ -1,19 +1,24 @@
-import Data.Either
+{-# LANGUAGE TemplateHaskell #-}
+import qualified Control.Lens as Lens
+import Control.Lens ((^.))
+import Data.Either (Either, lefts, rights)
 import Data.List (intercalate)
+import qualified Data.Map.Strict as Map
 import qualified Text.Parsec as Parsec
 import Text.Parsec ((<|>),(<?>))
 import Text.Printf (printf)
 
 type CircuitName = String
 type JugglerName = String
-type Foo = Either Circuit Juggler
+type FileLine = Either Circuit Juggler
 
 data Skill = Skill { _h :: Int, _e :: Int, _p :: Int }
 data Circuit = Circuit { _cName :: CircuitName, _cSkill :: Skill }
-data Juggler = Juggler { _jName :: JugglerName
-                       , _jSkill :: Skill
-                       , _jPref :: [CircuitName]
-                       }
+data Juggler = Juggler
+               { _jName :: JugglerName
+               , _jSkill :: Skill
+               , _jPref :: [CircuitName]
+               }
 
 instance Show (Skill) where
   show (Skill h e p) = printf "<%d %d %d>" h e p
@@ -25,26 +30,36 @@ instance Show (Juggler) where
   show (Juggler jn sk cns) = jn ++ " " ++ show sk ++ " " ++ showCNList cns
     where showCNList n = "[" ++ intercalate "," n ++ "]"
 
+
+data ProcessData = ProcessData
+                   { _circMap :: Map.Map CircuitName Circuit
+                   , _juggMap :: Map.Map JugglerName Juggler
+                   , _circuits :: Map.Map CircuitName [JugglerName]
+                   , _toProcess :: [JugglerName]
+                   } deriving (Show)
+
+-- Use TH calls to create our lenses
+Lens.makeLenses ''Skill
+Lens.makeLenses ''Circuit
+Lens.makeLenses ''Juggler
+Lens.makeLenses ''ProcessData
+
 str2Int s = read s :: Int
 
 parseSkill :: Parsec.Parsec String () Skill
 parseSkill = do
-  Parsec.string "H:"
+  Parsec.string " H:"
   h <- Parsec.many1 Parsec.digit
-  Parsec.spaces
-  Parsec.string "E:"
+  Parsec.string " E:"
   e <- Parsec.many1 Parsec.digit
-  Parsec.spaces
-  Parsec.string "P:"
+  Parsec.string " P:"
   p <- Parsec.many1 Parsec.digit
   return (Skill (str2Int h) (str2Int e) (str2Int p))
 
 parseCircuit :: Parsec.Parsec String () Circuit
 parseCircuit = do
-  Parsec.char 'C'
-  Parsec.spaces
+  Parsec.string "C "
   name <- Parsec.many1 Parsec.alphaNum
-  Parsec.spaces
   sk <- parseSkill
   return (Circuit name sk)
 
@@ -53,17 +68,14 @@ parseCL = Parsec.many1 Parsec.alphaNum `Parsec.sepBy` Parsec.char ','
 
 parseJuggler :: Parsec.Parsec String () Juggler
 parseJuggler = do
-  Parsec.char 'J'
-  Parsec.spaces
+  Parsec.string "J "
   name <- Parsec.many1 Parsec.alphaNum
-  Parsec.spaces
   sk <- parseSkill
-  Parsec.spaces
+  Parsec.space
   cl <- parseCL
   return (Juggler name sk cl)
 
-
-parseLine :: Parsec.Parsec String () Foo
+parseLine :: Parsec.Parsec String () FileLine
 parseLine = do
     c <- parseCircuit
     return (Left c)
@@ -72,8 +84,8 @@ parseLine = do
     return (Right j)
   <|> (eol >> parseLine)
 
-parseFile :: Parsec.Parsec String () [Foo]
-parseFile = Parsec.endBy parseLine eol
+parseLines :: Parsec.Parsec String () [FileLine]
+parseLines = Parsec.endBy parseLine eol
 
 eol =   Parsec.try (Parsec.string "\n\r")
     <|> Parsec.try (Parsec.string "\r\n")
@@ -81,15 +93,29 @@ eol =   Parsec.try (Parsec.string "\n\r")
     <|> Parsec.string "\r"
     <?> "end of line"
 
-main =
-    do c <- getContents
-       case Parsec.parse parseFile "(stdin)" c of
-            Left e -> do putStrLn "Error parsing input:"
-                         print e
-            Right r -> mapM_ print r
+processFile :: FilePath -> IO ()
+processFile f = do
+  c <- readFile f
+  case Parsec.parse parseLines f c of
+   Left e -> do putStrLn "Error parsing input:"
+                print e
+--   Right r -> mapM_ print $ doStuff r
+   Right r -> print $ doStuff r
 
--- Parsec.parse parseSkill "(stdin)" "H:5 E:7 P:24"
--- Parsec.parse parseCircuit "(stdin)" "C C1992 H:1 E:0 P:6"
--- Parsec.parse parseCircuitList "(stdin)" "C1223,C1325,C2352"
--- Parsec.parse parseJuggler "(stdin)"
---   "J J10000 H:8 E:8 P:2 C786,C896,C986,C1586,C1954,C1848,C326,C301,C443,C185"
+dotProductSk :: Skill -> Skill -> Int
+dotProductSk s t = s^.h*t^.h  +  s^.e*t^.e  +  s^.p*t^.p
+
+dotProductCrJg :: Circuit -> Juggler -> Int
+dotProductCrJg c j = dotProductSk (c^.cSkill) (j^.jSkill)
+
+mkCMap = Map.fromList . map (\c -> (c^.cName, c))
+mkOutM = Map.fromList . map (\c -> (c^.cName, []))
+mkJMap = Map.fromList . map (\j -> (j^.jName, j))
+
+doStuff f = d
+  where
+    circ = lefts f
+    jugg = rights f
+    toP = map (\j -> j^.jName) jugg
+    d = ProcessData (mkCMap circ) (mkJMap jugg) (mkOutM circ) toP
+
