ca0246629dd0a0454b6f5bc5674d84b62c41374d
Dan Nguyen <pedantic@gmail.com>
2016-08-16 00:32:08 -0500

    Add support for breaking cfor

--- kotlin/ForLoop/src/ForLoop.kt
+++ kotlin/ForLoop/src/ForLoop.kt
@@ -2,19 +2,31 @@
  * Created by dnn on 8/15/16.
  */
 
+class BreakException : Exception("This is a break exception.") {}
 
+/**
+ * Use "throw BreakException()" to perform a break, and return@cfor to perform a continue
+ */
 inline fun cfor(init: () -> Unit, cond: () -> Boolean, itr: () -> Unit, mainLoop: () -> Unit) {
     init()
-    while (cond()) {
-        mainLoop()
-        itr()
+    try {
+        while (cond()) {
+            mainLoop()
+            itr()
+        }
     }
-    return
+    catch (e: BreakException) {
+        // Do nothing. Just except the while loop
+    }
 }
 
 fun main(args: Array<String>) {
     var i = 0
     cfor({i = 3}, {i < 10}, {i++}) {
+        if (i > 7) {
+            println("loop ${i}")
+            throw BreakException()
+        }
         println("Hello World ${i}")
     }
 
