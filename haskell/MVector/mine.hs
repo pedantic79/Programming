769d55b4d360d0ebd3f583127f55a9bef7c4be29
Dan Nguyen <pedantic@gmail.com>
2017-05-22 10:56:01 -0500

    Solution to minesweeper codefight

---
+++ haskell/MVector/mine.hs
@@ -0,0 +1,61 @@
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
+import Control.Monad.ST
+import Control.Monad
+
+sweep = [[True,False,False,True],
+ [False,False,True,False],
+ [True,True,False,True]]
+
+create r c = do
+  v <- VM.new $ r * c + 2
+  let lls = replicate (r * c) 0
+  VM.write v 0 $ fromIntegral r
+  VM.write v 1 $ fromIntegral c
+  fill v 2 lls
+  return v
+  where
+    fill mv x lls = forM_ (zip [x..] lls) $ uncurry (VM.write mv)
+
+upPair mv ps = do
+  c <- VM.read mv 1
+  up mv (pair2idx c ps)
+  where
+    up mv = mapM_ (modify mv (+1))
+    pair2idx c = map (\(x,y) -> x * c + y + 2)
+
+modify mv f i = do
+  value <- VM.read mv i
+  VM.write mv i (f value)
+
+-- filterRange :: (Ord a, Num a) => a -> a -> [(a,a)] -> [(a,a)]
+filterRange r c = filter (\(x,y) -> x >= 0 && y >= 0 && x < r && y < c)
+
+-- genPairs :: Num a => a -> a -> [(a,a)]
+genPairs r c = [(r-1,c-1), (r-1,c), (r-1,c+1)
+               ,(r  ,c-1),          (r  ,c+1)
+               ,(r+1,c-1), (r+1,c), (r+1,c+1)
+               ]
+
+-- unf :: [[Bool]] -> V.Vector Int
+unf lls = runST $ do
+  let (r,c) = getSizeL lls
+  let mineVector = toVector lls
+  v <- create r c
+  forM_ [0..r-1] $ \i -> do
+    forM_ [0..c-1] $ \j -> do
+      when ((mineVector V.! i) V.! j)
+        (upPair v . filterRange r c $ genPairs i j)
+  V.freeze v
+  where
+    toVector = V.fromList . fmap V.fromList
+    getSizeL ls = (length ls, length (head ls))
+
+vector2List v = helper l
+  where
+    helper [] = []
+    helper ls = take c ls:helper (drop c ls)
+    c = v V.! 1
+    l = V.toList . V.drop 2 $ v
+
+test = (vector2List . unf $ sweep)
