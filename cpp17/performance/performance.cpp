56f9041f37262cebee9afb2970d9e2820974b935
Dan Nguyen <pedantic@gmail.com>
2018-05-20 23:37:15 -0500

    Add c++17 list vs vector performance

---
+++ cpp17/performance/performance.cpp
@@ -0,0 +1,110 @@
+#include <algorithm>
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <random>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+struct Timer {
+    Timer() : start_(clock_::now()) {}
+    void reset() { start_ = clock_::now(); }
+    auto elapsed() const {
+        return chrono::duration_cast<second_>(clock_::now() - start_).count();
+    }
+
+  private:
+    typedef chrono::high_resolution_clock clock_;
+    typedef chrono::duration<double, ratio<1>> second_;
+    chrono::time_point<clock_> start_;
+};
+
+template <typename T, typename S>
+void ordered_insert(T &container, const S &input) {
+    for (const auto &n : input) {
+        auto itr = begin(container);
+        for (; itr != end(container); itr++) {
+            if (n < *itr)
+                break;
+        }
+        container.insert(itr, n);
+    }
+}
+
+template <typename T, typename S>
+auto run(const vector<S> &input, int iterations) {
+    Timer t;
+    vector<decltype(t.elapsed())> r(iterations);
+
+    generate(begin(r), end(r), [&]() -> auto {
+        T container;
+        t.reset();
+        ordered_insert(container, input);
+        return t.elapsed();
+    });
+
+    return r;
+}
+
+template <typename T>
+string stats(const T &v) {
+    const auto sum = accumulate(begin(v), end(v), 0.0);
+    const auto m = sum / v.size();
+
+    const auto sqsum =
+        reduce(begin(v), end(v), 0.0, [=](const auto acc, const auto x) {
+            return acc + (x - m) * (x - m);
+        });
+
+    stringstream oss;
+    oss << fixed << " mean: " << m << " stdev: " << sqrt(sqsum / v.size())
+        << '\n';
+    return oss.str();
+}
+
+template <typename T>
+T get_default(const vector<T> &v, int n, const T &d) {
+    if (n < v.size()) {
+        T num = v[n];
+        return (num == 0) ? d : num;
+    } else {
+        return d;
+    }
+}
+
+template <typename T>
+vector<T> parse_argv(int argc, char const *argv[]) {
+    const vector<string> args(argv + 1, argv + argc);
+    vector<T> argi;
+    transform(begin(args), end(args), back_inserter(argi),
+              [](const auto &s) -> T {
+                  stringstream ss(s);
+                  T i;
+                  ss >> i;
+
+                  return i;
+              });
+    return argi;
+}
+
+int main(int argc, char const *argv[]) {
+    const auto argi = parse_argv<int>(argc, argv);
+    const auto len = get_default(argi, 0, 10000);
+    const auto times = get_default(argi, 1, 5);
+
+    vector<uint32_t> input(len);
+
+    mt19937 gen{random_device{}()};
+    generate(begin(input), end(input), gen);
+
+    const auto times_l = run<list<decltype(input)::value_type>>(input, times);
+    const auto times_v = run<vector<decltype(input)::value_type>>(input, times);
+
+    cout << input.size() << " integers " << times << " times\n";
+    cout << "list  :" << stats(times_l);
+    cout << "vector:" << stats(times_v);
+    return 0;
+}
